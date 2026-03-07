df3208081767845d24ba9fa62feb2e4b2afbf833
LandonHolligan <laho3191@colorado.edu>
2026-03-06 20:14:29 -0700

    Added transient plotting

--- P2_Task_1.m
+++ P2_Task_1.m
@@ -1,4 +1,4 @@
-clear all;
+clear;
 close all;
 clc;
 
@@ -13,44 +13,80 @@ alpha = k / (rho * cp);
 t1 = 1;
 t2 = 1000;
 
-L = 0.041275+0.0889+0.0254;
-T0 = 17.0501; %K
-x = 0.041275+0.0889;
+L = 0.041275 + 0.0889 + 0.0254;
+T0 = 17.0501; % K
+x = 0.041275 + 0.0889; % L
 
-t = [t1,t2];
+t = [t1, t2];
 
+%% Section 1: Temperature vs n iterations for two times
 
-    figure();
-    hold on;
-    grid on;
+figure();
+hold on;
+grid on;
 
-% n = 0:10
-for i = 1:2
+for i = 1:length(t)
+    
+    n_vals = 0:10;
+    u_vals = zeros(size(n_vals));
+    u_vals(1) = T0 + H_Al * x;
     summation = 0;
 
-    % Hold the number of modes and corresponding temperatures
-    n_vals = [];
-    u_vals = [];
-   
+    for n = 1:10
+        
+        lambda_n = ((2 * n - 1) * pi) / (2 * L);
+        bn = ((8 * L * H_Al) / (((2 * n) - 1)^2 * (pi^2))) * (-1)^n;
+        summation = summation + bn * sin(lambda_n * x) * exp(-(lambda_n^2) * alpha * t(i));
+        u_vals(n+1) = T0 + H_Al * x + summation;
 
-        for n = 1:10
-            lambdan = ((2 * n - 1) * pi) / (2 * L);
+    end
+
+    plot(n_vals, u_vals, '-o', 'LineWidth', 1.5);
+end
+
+xlabel('n Iterations');
+ylabel('T (K)');
+title('Temperature for Varying n at Two Times');
+legend(sprintf('t = %g s', t1), sprintf('t = %g s', t2), 'Location', 'best');
+hold off;
+
+
+%% Section 2: Temperature vs time for 8 different x values
+
+t_time = linspace(0, 3500, 3500);
+
+x_vals = linspace(0, x, 7);
+
+figure();
+hold on;
+grid on;
+
+n_chosen = 3;
+
+for j = 1:length(x_vals)
+    
+    x_current = x_vals(j);
+    T_time = zeros(size(t_time));
+
+    for i = 1:length(t_time)
+        summation = 0;
+
+        for n = 1:n_chosen
+            lambda_n = ((2 * n - 1) * pi) / (2 * L);
             bn = ((8 * L * H_Al) / (((2 * n) - 1)^2 * (pi^2))) * (-1)^n;
-    
-            summation = summation + bn * sin(lambdan * x) * exp(-(lambdan^2) * alpha * t(i));
-            u = T0 + H_Al * x + summation;
-    
-            n_vals = [0,n_vals, n];
-            u_vals = [T0+H_Al*x,u_vals, u];
- 
+
+            summation = summation + bn * sin(lambda_n * x_current) * exp(-(lambda_n^2) * alpha * t_time(i));
         end
-    
-    plot(n_vals, u_vals, '-o');
-    xlabel('n Iterations');
-    ylabel('T (K)');
-    title(sprintf('Steady State Temp for Varying n (Time = %g) seconds', t(i)));
 
-end    
+        T_time(i) = T0 + H_Al * x_current + summation;
+    end
 
-legend
+    plot(t_time, T_time, 'LineWidth', 1.5);
+end
+
+xlabel('Time (s)');
+ylabel('T (K)');
+title('Temperature vs Time for 8 Different x Values');
+legendStrings = arrayfun(@(k) sprintf('x_{%d} = %.4f m', k, x_vals(k)), 1:min(8,length(x_vals)), 'UniformOutput', false);
+legend(legendStrings, 'Location', 'best');
 hold off;
